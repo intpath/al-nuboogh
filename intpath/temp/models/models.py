b8bd9da15e43d5f6304c42fc57731064f98b046e
M-Saeb <Mohammed.saeb98@hotmail.com>
2022-05-22 10:56:21 +0300

    removed temp module

--- intpath/temp/models/models.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import models, fields, api
-
-class ResCompany(models.Model):
-    _inherit = "res.company"
-
-    include_iqd_pricing_in_accounting = fields.Boolean("Include IQD Pricing in Account Moves", copy=False)
